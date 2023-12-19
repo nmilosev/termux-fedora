772a1a3d23373b4c7989f5b58f4314bd502196c7
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2023-12-19 03:14:15 +0000

    updates
    updated the image to f39
    
    fixed the mkdir -p spello
    
    todo: add a rawhide source (Docker hub needs auth token)

--- termux-fedora.sh
+++ termux-fedora.sh
@@ -2,12 +2,11 @@
 
 # input validator and help
 
+# https://github.com/fedorapackaging/docker-images
+
 case "$1" in
-	f37)
-	    DOCKERIMAGE=https://download.fedoraproject.org/pub/fedora/linux/releases/37/Container/aarch64/images/Fedora-Container-Base-37-1.7.aarch64.tar.xz
-	    ;;
-	f38)
-	    DOCKERIMAGE=https://mirror.init7.net/fedora/fedora/linux/releases/38/Container/aarch64/images/Fedora-Container-Minimal-Base-38-1.6.aarch64.tar.xz
+	f39)
+	    DOCKERIMAGE=https://mirror.init7.net/fedora/fedora/linux/releases/39/Container/aarch64/images/Fedora-Container-Minimal-Base-39-1.5.aarch64.tar.xz
 	    ;;
 	uninstall)
 	    chmod -R 777 ~/fedora
@@ -28,7 +27,7 @@ pkg install proot tar wget -y
 
 # get the docker image
 
-mkdir ~/fedora
+mkdir -p ~/fedora
 cd ~/fedora
 /data/data/com.termux/files/usr/bin/wget $DOCKERIMAGE -O fedora.tar.xz
 
@@ -47,7 +46,7 @@ rm layer.tar
 rm fedora.tar.xz
 
 # fix DNS
-mkdir -P ~/fedora/etc/systemd/resolved.conf.d/
+mkdir -p ~/fedora/etc/systemd/resolved.conf.d/
 echo "DNS=185.95.218.42 185.95.218.43" > ~/fedora/etc/systemd/resolved.conf.d/DNS-Overwrite.conf
 echo "FallbackDNS=78.46.244.143 45.91.92.121" >> ~/fedora/etc/systemd/resolved.conf.d/DNS-Overwrite.conf
 
